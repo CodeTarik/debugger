06f97bb18289e89fad3dae351598d8edca4dc708
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-21 19:01:09 +0200

    second variant global scope

--- scopes.js
+++ scopes.js
@@ -1,8 +1,11 @@
 
-let testScopeVar = "hello world";
+let testScopeVar = "hello world"; //here it is defined globally - that's how it would work
 
 function scopeTest(){
-    console.log(testScopeVar);
-   
-    
+    scopeTestSecond();
 }    
+
+
+function scopeTestSecond(){
+    console.log(testScopeVar);
+}
